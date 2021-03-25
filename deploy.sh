33594c1070da8628ed8ad33cdfa923976c1a5397
Thawisap prathammate <thawisap.numnoii@gmail.com>
2021-03-25 16:49:36 +0700

    deploy
    Signed-off-by: Thawisap prathammate <thawisap.numnoii@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:iamlookod/iamlookod.github.io.git master
+git push -f git@github.com/Thawisap/thawisap.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:iamlookod/iamlookod.github.io.git main:gh-pages
